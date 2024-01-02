d3d0717e2822a1d4f9a995705342073d20d5361c
Arun Kumar <arunkumar.js25@gmail.com>
2024-01-02 22:46:29 +0530

    Update 01 Create Groups in Partner Orgs.py
    Added page skip

--- 01 Create Groups in Partner Orgs.py
+++ 01 Create Groups in Partner Orgs.py
@@ -1,6 +1,6 @@
 """
 Author: FellowBeginner
-Blog: https://fellowbeginners.wordpress.com
+Blog: https://fellowbeginners.wordpress.com/
 
 Automation Script: Create groups in partner organisations in CPQ instance
 Application: Oracle CPQ
@@ -18,20 +18,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-cpqInstanceName = "XXXXXX"
-username = "XYZ"
-password = ""
-chromedriverPath = "chromedriver.exe"
+cpqInstanceName = "XXX"
+username = "usrname"
+password = "pssword"
+chromedriverPath = ".\driver\chromedriver.exe" #path to selenium chromium driver
 
-url = "https://"+cpqInstanceName+".bigmachines.com"
-proxylogout = url+"/logout.jsp?proxy_logout=true&amp;_bm_trail_refresh_=true"
+#Basic URL used
+url = "https://" + cpqInstanceName + ".bigmachines.com"
+proxylogout = url + "/logout.jsp?proxy_logout=true&amp;_bm_trail_refresh_=true"
 exceptionListOfCompanies = []
 
-pagesCountStr = driver.find_element_by_xpath("//table[3]//td[@class='bottom-bar']").text
-pagesCount = int(pagesCountStr.split(": ")[1])//100
-
-def createGroup(companyName, groupName, groupVarName):
-	if (len(driver.find_elements_by_xpath("//td[text()='"+groupVarName+"']")) == 0):
+def createGroup(companyName, groupName, groupVarName, addAllUsers = False):
+    if (len(driver.find_elements_by_xpath("//td[text()='" + groupVarName + "']")) == 0):
         driver.find_element_by_xpath("//a[@id='add']").click()
         time.sleep(5)
         element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "group-name|input")))
@@ -39,63 +37,76 @@ def createGroup(companyName, groupName, groupVarName):
         time.sleep(3)
         driver.find_element_by_id("variable-name|input").send_keys(groupVarName)
         time.sleep(3)
+        if(addAllUsers): #Click >> arrow button to add all users to the group
+            driver.find_element_by_id("all-users-right-btn_oj2|text").click()  # select all users
+            time.sleep(3)
         driver.find_element_by_id("save-btn_oj0|text").click()
         time.sleep(5)
-        print("Group Partner Manager added for " + companyName)
+        print("Group "+groupName+" added for " + companyName)
     else:
-        print("Group Partner Manager already exists for " + companyName)
+        print("Group "+groupName+" already exists for " + companyName)
 
-
-#START
+# START
 driver = webdriver.Chrome(chromedriverPath)
 driver.implicitly_wait(1)
 driver.maximize_window()
-driver.get("https://"+cpqInstanceName+".bigmachines.com/admin/company/list_external_companies.jsp")
+driver.get("https://" + cpqInstanceName + ".bigmachines.com/admin/company/list_external_companies.jsp")
 driver.find_element_by_id("username").send_keys(username)
 driver.find_element_by_id("psword").send_keys(password)
 driver.find_element_by_id("log_in").click()
 
-#Skip the pages
+pagesCountStr = driver.find_element_by_xpath("//table[3]//td[@class='bottom-bar']").text
+pagesCount = int(pagesCountStr.split(": ")[1]) // 100
+
+# Skip the pages
 noOfPagesToSkip = 0;
+'''
 for j in range(noOfPagesToSkip):
     ActionChains(driver).key_down(Keys.CONTROL).key_down(Keys.END).key_up(Keys.CONTROL).perform()
     driver.find_element_by_id("next_iter_link").click()
     time.sleep(5)
+'''
 
-#Extract all the partner orgs available in the page
-for i in range(pagesCount+1):
-    print("pageNo :",i)
+# Extract all the partner orgs available in the page
+for i in range(pagesCount + 1):
+    print("pageNo :", i)
 
-    count=1
-    list_of_companies = [[x.get_attribute('href'),x.text] for x in driver.find_elements_by_xpath("//td[1]//a[@class='list-field']")]
-    list_of_companiesProxy = [x.get_attribute('href') for x in driver.find_elements_by_xpath("//td[8]//a[@class='list-field']")]
+    if(i<noOfPagesToSkip):
+        continue
 
-    for company,proxyLink in zip(list_of_companies,list_of_companiesProxy):
-            companyID = company[0].replace("https://"+cpqInstanceName+".bigmachines.com/admin/company/edit_external_company.jsp?id=","")
-            companyName = company[1]
-            print (count,companyName)
-			
-			# Check if the partners arepresent in exceptionList
-            if companyID not in exceptionListOfCompanies:
-                #List of Users: print(url+"/admin/users/list_users.jsp?company_id="+companyID)
-                #ProxyLogin: print(proxyLink)
-                driver.get(proxyLink)
-                time.sleep(2)
-                driver.get(url+"/admin/groups/list_groups.jsp?_bm_trail_refresh_=true")
-                time.sleep(2)
+    count = 1
+    list_of_companies = [[x.get_attribute('href'), x.text] for x in
+                         driver.find_elements_by_xpath("//td[1]//a[@class='list-field']")]
+    list_of_companiesProxy = [x.get_attribute('href') for x in
+                              driver.find_elements_by_xpath("//td[8]//a[@class='list-field']")]
 
-                createGroup(companyName, "Group A", "groupA")
-                createGroup(companyName, "Group B", "groupB")
+    for company, proxyLink in zip(list_of_companies, list_of_companiesProxy):
+        companyID = company[0].replace(
+            "https://" + cpqInstanceName + ".bigmachines.com/admin/company/edit_external_company.jsp?id=", "")
+        companyName = company[1]
+        print(count, companyName)
 
-                driver.get(proxylogout)
-                time.sleep(5)
-            else:
-                    print("This company "+companyName+" is in exclusion list..")
+        # Check if the partners arepresent in exceptionList
+        if companyID not in exceptionListOfCompanies:
+            # List of Users: print(url+"/admin/users/list_users.jsp?company_id="+companyID)
+            # ProxyLogin: print(proxyLink)
+            driver.get(proxyLink)
+            time.sleep(2)
+            driver.get(url + "/admin/groups/list_groups.jsp?_bm_trail_refresh_=true")
+            time.sleep(2)
 
-            count=count+1
+            createGroup(companyName, "Partner User", "partnerUser",True)
+            createGroup(companyName, "Partner Manager", "partnerManager")
 
-    if(count > 100):
-        for j in range(i+1):
+            driver.get(proxylogout)
+            time.sleep(5)
+        else:
+            print("This company " + companyName + " is in exclusion list..")
+
+        count = count + 1
+
+    if (count > 100):
+        for j in range(i + 1):
             ActionChains(driver).key_down(Keys.CONTROL).key_down(Keys.END).key_up(Keys.CONTROL).perform()
             driver.find_element_by_id("next_iter_link").click()
             time.sleep(5)
